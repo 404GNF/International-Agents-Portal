a4f256122bcf291af72d521488f2b46a3777bd5a
Andrew Rozs <rozs0001@hz.nl>
2021-04-04 15:20:07 +0200

    backup before testing

--- resources/views/common/welcome.blade.php
+++ resources/views/common/welcome.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HZ Agency Webshop</title>
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"
     />
+
+    <link rel="stylesheet" href="{{ asset('css/resources.css') }}" />
 
 </head>
 
