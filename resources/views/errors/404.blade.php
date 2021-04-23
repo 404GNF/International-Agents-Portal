5ac95809e2a1da289b43ff5a8aba930d6efbfc8b
AxelZublena <zubl0001@hz.nl>
2021-04-23 20:51:50 +0200

    Added 404 and 500 pages functionalities

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,16 @@
 @extends('common.welcome')
 
 @section('content')
-    <section class="hero is-medium is-bold is-link is-fullheight">
+    <section class="hero is-medium is-bold is-link is-fullheight is-overlay">
         <div class="hero-body">
             <div class="container">
                 <p class="title is-1">404 Error</p>
                 <p class="mb-6 subtitle is-1">The page was not found</p>
-                <p class="mb-1 subtitle is-3 has-text-white">The link you clicked might be out dated.<br>Try return to the previous page, or simply go to the home page.</p>
+                <p class="mb-1 subtitle is-3 has-text-white">The link you clicked might be out dated.<br>Try to return to the previous page, or simply go to the home page.</p>
                 <div class="mt-6">
                     <div class="mt-6 buttons are-large">
-                        <a class="has-text-white button is-outlined is-size-3 mr-4" style="background: #ee7a00" href="#">Return Back</a>
-                        <a class="has-text-white button is-outlined is-size-3 ml-4" style="background: #3298dc" href="#">Go Home</a>
+                        <button class="has-text-white button is-outlined is-size-3 mr-4 px-6" style="background: #ee7a00" onclick="window.history.back()">Back</button>
+                        <a class="has-text-white button is-outlined is-size-3 ml-4 px-6" style="background: #3298dc" href="/">Home</a>
                     </div>
                 </div>
             </div>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,7 @@
 @extends('common.welcome')
 
 @section('content')
-    <section class="hero is-medium is-bold is-link is-fullheight">
+    <section class="hero is-medium is-bold is-link is-fullheight is-overlay">
         <div class="hero-body">
             <div class="container">
                 <p class="title is-1">500 Error</p>
@@ -9,9 +9,9 @@
                 <p class="mb-1 subtitle is-3 has-text-white">Our server encountered a problem.<br>Try reloading the page, returning to the previous page, or simply to the home page.</p>
                 <div class="mt-6">
                     <div class="mt-6 buttons are-large">
-                        <a class="has-text-white button is-outlined is-size-3 mr-4" style="background: #3298dc" href="#">Back</a>
-                        <a class="has-text-white button is-outlined is-size-3 mr-4 ml-4" style="background: #ee7a00" href="#">Reload</a>
-                        <a class="has-text-white button is-outlined is-size-3 ml-4" style="background: #3298dc" href="#">Home</a>
+                        <a class="has-text-white button is-outlined is-size-3 mr-4 px-6" style="background: #3298dc" onclick="window.history.back()">Back</a>
+                        <a class="has-text-white button is-outlined is-size-3 mx-4 px-6" style="background: #ee7a00" href="{{ url()->current() }}">Reload</a>
+                        <a class="has-text-white button is-outlined is-size-3 ml-4 px-6" style="background: #3298dc" href="/">Home</a>
                     </div>
                 </div>
             </div>
