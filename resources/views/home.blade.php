bdc6cdc1a93952546b912849e6f049e0d47d839a
Bogsuna <70523908+Bogsuna@users.noreply.github.com>
2021-06-09 14:07:07 +0300

    Implementing button for managing personal stories and validation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,19 @@
             <div class="container">
                 <p class="title is-2">Home</p>
                 <p class = 'subtitle is-3'>Personal Stories</p>
-                <div onclick="window.location.href='/admin/personal-stories'" class="button is-primary">Manage Personal Stories</div>
+{{--                checks whether the user is logged and shows the button if he is--}}
+                @if ($user = Auth::user())
+                <div onclick="window.location.href='/admin/personal-stories'" class="button is-primary">
+                    Manage Personal Stories
+                </div>
+                @endif
             </div>
         </div>
     </section>
 
+{{--    Goes through all of the personal stories and creates a div for each of them that consists heading and image that--}}
+{{--    is link to another webpage--}}
     @foreach($stories as $story)
-
             <div class="personal-stories">
                 <h2 class="personal-stories-titles" >{{$story->title}}</h2>
                 <a href="{{$story->url}}">
