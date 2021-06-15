d2222d17a10f5a659c3926da0f6b884a9405df03
Bogsuna <70523908+Bogsuna@users.noreply.github.com>
2021-06-15 16:34:15 +0300

    changing the hero and footer

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer has-background-info-light">
     <div class="content has-text-centered">
       <p>
         <strong>International Agents Portal</strong> made by <a target="_blank" href="https://github.com/404GNF">404GNF</a> team from HZ University of Applied Sciences | All Rights Reserved
