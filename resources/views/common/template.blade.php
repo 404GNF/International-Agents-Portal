0dfd6c12fd08d7bf35ec3c5c78d95fad2d29ad04
TophatPigeonno <sche0177@hz.nl>
2021-06-18 19:04:37 +0200

    added script again for the old socials tab so I can remove it later

--- resources/views/common/template.blade.php
+++ resources/views/common/template.blade.php
@@ -26,6 +26,19 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
 <script type="text/javascript" src="{{ asset('js/functions.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.16.6/sweetalert2.min.js"></script>
-
+<script>
+    // Social media menu hide/show
+    const menu = document.getElementById("social");
+    const hideButton = document.getElementById("hide-social");
+    const showButton = document.getElementById("show-social");
+    hideButton.addEventListener("click", ()=>{
+        menu.style.visibility = "hidden";
+        showButton.style.visibility = "visible";
+    });
+    showButton.addEventListener("click", ()=>{
+        menu.style.visibility = "visible";
+        showButton.style.visibility = "hidden";
+    });
+</script>
 </body>
 </html>
